154be30f190d2a42c0e02cb0a7999094ed2f6ec0
Ryan Turner <98337469+rcturner7@users.noreply.github.com>
2023-09-17 22:09:12 -0500

    Update myGradeCalculator.c

--- myGradeCalculator.c
+++ myGradeCalculator.c
@@ -1,24 +1,38 @@
 #include<stdio.h>
 
+// Starting the program and functions
 int main() {
     int myPoints;
     int pointsPossible;
     float gradePercentage;
 
+    // Asking user total points earned in class
     printf("What is your total points in class accrued?\n");
     scanf("%d", &myPoints);
+
+    // Extra space
     printf("\n");
+
+    // printing off total points user entered to test function worked properly
     printf("total points: %d\n", myPoints);
 
+    // Asking user total points possible
     printf("What is total points possible you can earn?\n");
     scanf("%d", &pointsPossible);
+
+    // extra space
     printf("\n");
+
+    // printing total points possible user entered to test function worked properly
     printf("total points possible: %d\n", pointsPossible);
 
+    // dividing (myPoints) by (pointsPossible) then multiplying by 100 to get grade percentage
     gradePercentage = (float) myPoints / pointsPossible * 100;
 
+    // printing the user's grade from equation to test function worked properly
     printf("Your grade is %.2f percent.\n", gradePercentage);
 
+    // using if/else if/ else statements in percentage range to print out what letter grade the user is receiving
     if (gradePercentage >= 90.00)
     {
         printf("You have an A in class.\n");
@@ -38,6 +52,6 @@ int main() {
     else
         printf("YOu have a F in the class and you are failing. Seek help immediatley and don't fuck around.\n");
 
-    return 1;  
+    return 0;  
     
 }
